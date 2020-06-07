a7791617a5d8a03efd6122d071454adfa77a5780
RHRivasG <51801113+RHRivasG@users.noreply.github.com>
2020-06-07 06:57:50 -0400

    .

--- protocol/general.go
+++ protocol/general.go
@@ -4,7 +4,11 @@ import (
 	port "serialport-protocol/protocol/ports"
 )
 
-func comnunicate(w port.Writer, l port.Listener, message []string) []string {
+func Converse(w port.Writer, l port.Listener, message []string) []string {
 	w.Writing(message)
 	return l.Listening()
+}
+
+func Finalize(w port.Writer, lastMessage string) {
+	w.Writer(lastMessage)
 }
